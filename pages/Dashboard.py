e66bfc694fe2bdde7b529ef5b22201fd1af58a98
AJustinD <111726354+AJustinD@users.noreply.github.com>
2024-04-03 19:32:09 +0700

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -98,7 +98,7 @@ plot_raw_data()
 
 # Predict forecast with Prophet.
 df_train = data[['Date','Close']]
-df_train = df_train.rename(columns={"Date": "Date", "Close": "Price"})
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
 m = Prophet()
 m.fit(df_train)
